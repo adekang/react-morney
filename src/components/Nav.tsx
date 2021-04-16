a9b293ed7e7e1a4493dcb3ddc674a06603f39628
TopLong <201497709@qq.com>
2021-04-16 17:30:09 +0800

    搞定 icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,12 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
+// SVG 引入
+require('icons/money.svg');
+require('icons/statistics.svg');
+require('icons/bookkeeping.svg');
+
+
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0, 0, 0, .25);
   line-height: 24px;
@@ -12,8 +18,18 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.3%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
+  }
 `;
 
 const Nav = () => {
@@ -21,12 +37,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money"/>
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#bookkeeping"/>
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#statistics"/>
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
