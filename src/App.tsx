dce736dea816f3a3892100052d97ce3219862b4e
TopLong <201497709@qq.com>
2021-04-16 11:04:24 +0800

    路由配置OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,69 @@
 import React from 'react';
-import './App.scss';
 
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from="/" to="/money"/>
+
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+
+function NoMatch() {
+  return (
     <div>
-      123
+      <h3>
+        页面不存在 404
+      </h3>
     </div>
   );
 }
