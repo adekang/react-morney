3379ac6e5b473f3888c04561e0f9a9070e7e26af
TopLong <201497709@qq.com>
2021-04-19 19:27:59 +0800

    编辑 tag 名

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -11,10 +11,26 @@ const defaultTags = [
 const useTags = () => {
   const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+  const findTagIndex = (id: number) => {
+    let result = -1;
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i].id === id) {
+        result = i;
+        break;
+      }
+    }
+    return result;
+  };
+  const updateTag = (id: number, obj: { name: string }) => {
+    const index = findTagIndex(id);
+    // 深拷贝 tags 得到 tagsClone
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    // 把 tagsClone 的第 index 删掉，换成 {id:id, name: obj.name}
+    tagsClone.splice(index, 1, {id: id, name: obj.name});
+    setTags(tagsClone);
+  };
   return {
-    tags,
-    setTags,
-    findTag
+    tags, setTags, findTag, updateTag, findTagIndex
   };
 };
 
