9f519635d1bf56f3b7ea856ff2f926a0aa495c32
TopLong <201497709@qq.com>
2021-04-20 20:23:46 +0800

    引入 dayjs 统计页面样式 OK

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -11,7 +11,9 @@ const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
 `;
-
+const CategoryWrapper = styled.div`
+  background: #c4c4c4;
+`;
 type Category = '-' | '+'
 
 const defaultFormData = {
@@ -40,8 +42,10 @@ function Money() {
                    onChange={tagIds => onChange({tagIds})}/>
       <NoteSection value={selected.note}
                    onChange={note => onChange({note})}/>
-      <CategorySection value={selected.category}
-                       onChange={category => onChange({category})}/>
+      <CategoryWrapper>
+        <CategorySection value={selected.category}
+                         onChange={category => onChange({category})}/>
+      </CategoryWrapper>
       <NumberPadSection value={selected.amount}
                         onChange={amount => onChange({amount})}
                         onOk={submit}/>
